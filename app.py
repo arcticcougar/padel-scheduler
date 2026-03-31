24d5bba7417e15dd515bfb2ad396f0ae395a666b
ArcticCougar <scott_adam2010@yahoo.co.uk>
2026-03-31 21:08:22 +0100

    Players: rename Peter to Pieter

--- app.py
+++ app.py
@@ -753,7 +753,7 @@ def main():
         {"name": "Outi", "gender": "F", "skill": 5},
         {"name": "Paola", "gender": "F", "skill": 4},
         {"name": "Paul", "gender": "M", "skill": 7},
-        {"name": "Peter", "gender": "M", "skill": 6},
+        {"name": "Pieter", "gender": "M", "skill": 6},
         {"name": "Ray", "gender": "M", "skill": 7},
         {"name": "Roger", "gender": "M", "skill": 6},
         {"name": "Ronan", "gender": "M", "skill": 7},
